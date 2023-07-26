ca262e95f926f8710d8ef7c349001e20d07072c9
Cauê Diademi <caue.diademi@hotmail.com>
2023-07-26 12:26:22 -0300

    master: datepicker component changed

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,49 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { View, StyleSheet, Text } from 'react-native';
-import ReactDatePicker from 'react-native-datepicker';
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DatePicker from "react-native-date-picker";
 
-const DEFAULT_DATE_FORMAT = 'YYYY-MM-DD';
-const CONFIRM_BUTTON_TEXT = 'Confirm';
-const CANCEL_BUTTON_TEXT = 'Cancel';
+const DEFAULT_DATE_FORMAT = "YYYY-MM-DD";
+const CONFIRM_BUTTON_TEXT = "Confirm";
+const CANCEL_BUTTON_TEXT = "Cancel";
 
 export default function Datepicker(props) {
-  const {
-    name, value, meta, style, onChangeInputValue, isMandatory
-  } = props;
+	const { name, value, meta, style, onChangeInputValue, isMandatory } = props;
 
-  return (
-    <View style={style}>
-      <Text style={styles.text}>{`${meta.text || meta.title} ${isMandatory ? '*' : ''}`}</Text>
-      <ReactDatePicker
-        key={name}
-        style={styles.date}
-        date={value}
-        mode="date"
-        format={DEFAULT_DATE_FORMAT}
-        confirmBtnText={CONFIRM_BUTTON_TEXT}
-        cancelBtnText={CANCEL_BUTTON_TEXT}
-        onDateChange={onChangeInputValue}
-      />
-    </View>
-  );
+	const [show, setShow] = useState(false);
+
+	return (
+		<View style={style}>
+			<Text style={styles.text}>{`${meta.text || meta.title} ${
+				isMandatory ? "*" : ""
+			}`}</Text>
+			<DatePicker
+				key={name}
+				modal={true}
+				open={show}
+				date={value}
+				mode={"date"}
+				is24hourSource={true}
+				onConfirm={onChangeInputValue}
+				confirmText={CONFIRM_BUTTON_TEXT}
+				cancelText={CANCEL_BUTTON_TEXT}
+				onCancel={() => {
+					setShow(false);
+				}}
+				style={styles.date}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  text: {
-    margin: 10,
-  },
-  date: {
-    width: '97%',
-    marginLeft: 10,
-  },
+	text: {
+		margin: 10,
+	},
+	date: {
+		width: "97%",
+		marginLeft: 10,
+	},
 });
 
 Datepicker.propTypes = {
-  name: PropTypes.string.isRequired,
-  meta: PropTypes.object.isRequired,
-  value: PropTypes.string,
-  style: PropTypes.object,
-  onChangeInputValue: PropTypes.func,
-  isMandatory: PropTypes.bool
+	name: PropTypes.string.isRequired,
+	meta: PropTypes.object.isRequired,
+	value: PropTypes.string,
+	style: PropTypes.object,
+	onChangeInputValue: PropTypes.func,
+	isMandatory: PropTypes.bool,
 };
